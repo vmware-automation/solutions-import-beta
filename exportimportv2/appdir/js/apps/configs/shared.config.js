06dcfe9a6fa983efef1f4d48ea5a65fdc9c7f4eb
Samuel Doyle <samueldoyle@vmware.com>
2012-10-02 18:50:42 -0700

    Place jquery in head, remove more unused

--- exportimportv2/appdir/js/apps/configs/shared.config.js
+++ exportimportv2/appdir/js/apps/configs/shared.config.js
@@ -3,9 +3,6 @@
  * Shared common config used among pages if not all this needed make another
  */
 TESTING = true;
-if (typeof window.postMessage === "undefined") {
-    alert("postMessage not supported in this browser");
-}
 
 ALERT_ERROR_CLASSES = "alert alert-error";
 ALERT_SUCCESS_CLASSES = "alert alert-success";
@@ -30,12 +27,6 @@ requirejs.config({
         "jqPlugins/jquery.activity-indicator":["jquery"],
         "jqPlugins/jquery.validate":["jquery"],
         "jqPlugins/jquery.validate.bootstrap":["jquery", "jqPlugins/jquery.validate"],
-        "jqPlugins/postmessage":{
-            deps:["jquery"],
-            exports:function ($) {
-                return this.pm;
-            }
-        },
         "thirdparty/crypto-min":{},
         "underscore":{
             exports:"_"
@@ -45,12 +36,8 @@ requirejs.config({
             exports:"Backbone"
         },
         "twitterjs/bootstrap-min":["jquery"]
-       /* "twitterjs/bootstrap-transition":["jquery"],
-        "twitterjs/bootstrap-button":["jquery"],
-        "twitterjs/bootstrap-collapse":["jquery"],
-        "twitterjs/bootstrap-modal":["jquery"]*/
     },
-    deps:["jquery", "util/appDirCommon", "jqPlugins/jquery.validate", "jqPlugins/jquery.validate.bootstrap", "jqPlugins/postmessage",
+    deps:["jquery", "util/appDirCommon", "jqPlugins/jquery.validate", "jqPlugins/jquery.validate.bootstrap",
           "jqPlugins/jquery.activity-indicator", "jqPlugins/jquery.url", "thirdparty/crypto-min", "twitterjs/bootstrap.min"], // force some to load since they aren't directly referenced
     callback:function ($, cu) {
         // Add a domain specifier check to the validator
