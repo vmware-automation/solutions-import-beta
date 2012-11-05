03b0d234fc13a254483129c224f1f48cb39a68c4
samueldoyle <samueldoyle@vmware.com>
2012-11-05 00:37:14 -0800

    Add support for optional markdown Switch to generated html from markdown after import if provided, defaults to default info Add optional console logging for deployed version

--- exportimport-deploy/appdir/js/apps/configs/shared.config.js
+++ exportimport-deploy/appdir/js/apps/configs/shared.config.js
@@ -4,5 +4,19 @@ ALERT_SUCCESS_CLASSES = "alert alert-success";
 
 requirejs.config({
     baseUrl:"../js", 
-    waitSeconds: 15
+    waitSeconds: 15,
+    callback:function () {
+        // Assertion from: http://aymanh.com/9-javascript-tips-you-may-not-know#assertion
+        function AssertException(message) {
+            this.message = message;
+        }
+        AssertException.prototype.toString = function () {
+            return 'AssertException: ' + this.message;
+        };
+        window.assert = function assert(exp, message) {
+            if (!exp) {
+                throw new AssertException(message);
+            }
+        }
+    }
 });
